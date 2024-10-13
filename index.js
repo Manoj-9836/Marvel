b4e42a60642b5a0b0971d5df7d060affb1e2ecc5
Manoj-9836 <108039744+Manoj-9836@users.noreply.github.com>
2024-10-13 11:39:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,201 @@
+$(function() {
+    $('a.main-nav').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+function navigate() {
+    window.location.href = 'https://www.hotstar.com/in/movies/iron-man/1660000038?filters=content_type%3Dmovie';
+  }
+
+  function navigate1() {
+    window.location.href = 'https://www.hotstar.com/in/movies/avengers-infinity-war/1660010677';
+  }
+
+  function navigate2() {
+    window.location.href = 'https://www.hotstar.com/in/movies/thor/1660000044?filters=content_type%3Dmovie';
+  }
+
+  function navigate3() {
+    window.location.href = 'https://www.hotstar.com/in/movies/captain-america-the-first-avenger/1660000034?filters=content_type%3Dmovie';
+  }
+
+  function navigate4() {
+    window.location.href = 'https://www.hotstar.com/in/movies/marvels-the-avengers/1660000015?filters=content_type%3Dmovie';
+  }
+
+  function navigate5() {
+    window.location.href = 'https://www.hotstar.com/in/movies/iron-man-3/1660000042?filters=content_type%3Dmovie';
+  }
+
+  function navigate6() {
+    window.location.href = 'https://www.hotstar.com/in/movies/thor-the-dark-world/1260018142?filters=content_type%3Dmovie';
+  }
+
+  function navigate7() {
+    window.location.href = 'https://www.hotstar.com/in/movies/captain-america-the-winter-soldier/1260016410?filters=content_type%3Dmovie';
+  }
+
+  function navigate8() {
+    window.location.href = 'https://www.hotstar.com/in/movies/guardians-of-the-galaxy/1260018294?filters=content_type%3Dmovie';
+  }
+
+  function navigate9() {
+    window.location.href = 'https://www.hotstar.com/in/movies/captain-america-civil-war/1260016768?filters=content_type%3Dmovie&is_paywall_onboarding=true';
+  }
+
+  function navigate10() {
+    window.location.href = 'https://www.hotstar.com/in/movies/doctor-strange/1260018179?filters=content_type%3Dmovie&is_paywall_onboarding=true';
+  }
+
+  function navigate11() {
+    window.location.href = 'https://www.hotstar.com/in/movies/guardians-of-the-galaxy-vol-2/1260018295?filters=content_type%3Dmovie&is_paywall_onboarding=true';
+  }
+
+  function navigate12() {
+    window.location.href = 'https://www.hotstar.com/in/movies/spider-man-homecoming/1260104626?filters=content_type%3Dmovie&is_paywall_onboarding=true';
+  }
+
+  function navigate13() {
+    window.location.href = 'https://www.hotstar.com/in/movies/thor-ragnarok/1660010577?filters=content_type%3Dmovie&is_paywall_onboarding=true';
+  }
+
+  function naviate14() {
+    window.location.href = 'https://www.hotstar.com/in/movies/black-panther/1660010672?filters=content_type%3Dmovie&is_paywall_onboarding=true';
+  }
+
+  function navigate15() {
+    window.location.href = 'https://www.hotstar.com/in/movies/avengers-infinity-war/1660010677?filters=content_type%3Dmovie&is_paywall_onboarding=true';
+  }
+
+  function navigate16() {
+    window.location.href = 'https://www.hotstar.com/in/movies/ant-man-and-the-wasp/1660010696?filters=content_type%3Dmovie&is_paywall_onboarding=true';
+  }
+
+  function navigate17() {
+    window.location.href = 'https://www.hotstar.com/in/movies/captain-marvel/1260014878?filters=content_type%3Dmovie&is_paywall_onboarding=true';
+  }
+
+  function navigate18() {
+    window.location.href = 'https://www.hotstar.com/in/movies/avengers-endgame/1260013556?filters=content_type%3Dmovie&is_paywall_onboarding=true';
+  }
+
+  function navigate19() {
+    window.location.href = 'https://www.hotstar.com/in/shows/wandavision/1260051344?filters=content_type%3Dshow_clips';
+  }
+
+  function navigate20() {
+    window.location.href = 'https://www.hotstar.com/in/shows/the-falcon-and-the-winter-soldier/1260055670?filters=content_type%3Dshow_clips';
+  }
+
+  function navigate21() {
+    window.location.href = 'https://www.hotstar.com/in/shows/loki/1260063451?filters=content_type%3Dshow_clips';
+  }
+
+  function navigate22() {
+    window.location.href = 'https://www.hotstar.com/in/movies/black-widow/1260067485?filters=content_type%3Dmovie';
+  }
+
+  function navigate23() {
+    window.location.href = 'https://www.hotstar.com/in/shows/what-if/1260066057?filters=content_type%3Dshow_clips';
+  }
+  
+  function navigate24() {
+    window.location.href = 'https://www.hotstar.com/in/movies/shang-chi-and-the-legend-of-the-ten-rings/1260072682?filters=content_type%3Dmovie';
+  }
+
+  function navigate25() {
+    window.location.href = 'https://www.hotstar.com/in/movies/eternals/1260077949?filters=content_type%3Dmovie';
+  }
+
+  function navigate26() {
+    window.location.href = 'https://www.hotstar.com/in/shows/hawkeye/1260073683?filters=content_type%3Dshow_clips';
+  }
+
+  function navigate27() {
+    window.location.href = 'https://www.hotstar.com/in/shows/moon-knight/1260089681?filters=content_type%3Dshow_clips';
+  }
+
+  function navigate28() {
+    window.location.href = 'https://www.hotstar.com/in/movies/doctor-strange-in-the-multiverse-of-madness/1260103761?filters=content_type%3Dmovie';
+  }
+
+  function navigate29() {
+    window.location.href = 'https://www.hotstar.com/in/shows/ms-marvel/1260098459?filters=content_type%3Dshow_clips';
+  }
+
+  function navigate30() {
+    window.location.href = 'https://www.hotstar.com/in/movies/thor-love-and-thunder/1260110008?filters=content_type%3Dmovie';
+  }
+
+  function navigate31() {
+    window.location.href = 'https://www.hotstar.com/in/shows/she-hulk-attorney-at-law/1260103788?filters=content_type%3Dshow_clips';
+  }
+
+  function navigate32() {
+    window.location.href = 'https://www.hotstar.com/in/movies/werewolf-by-night/1260111093?filters=content_type%3Dmovie';
+  }
+
+  function navigate33() {
+    window.location.href = 'https://www.hotstar.com/in/movies/black-panther-wakanda-forever/1260118821?filters=content_type%3Dmovie';
+  }
+
+  function navigate34() {
+    window.location.href = 'https://www.hotstar.com/in/movies/the-guardians-of-the-galaxy-holiday-special/1260111097?filters=content_type%3Dmovie';
+  }
+
+  function navigate35() {
+    window.location.href = 'https://www.hotstar.com/in/movies/ant-man-and-the-wasp-quantumania/1260140795?filters=content_type%3Dmovie';
+  }
+
+  function naviate36() {
+    window.location.href = 'https://www.hotstar.com/in/shows/secret-invasion/1260111182?filters=content_type%3Dshow_clips';
+  }
+
+  function navigate37() {
+    window.location.href = 'https://www.hotstar.com/in/movies/spider-man/1221006195?filters=content_type%3Dmovie';
+  }
+
+  function navigate38() {
+    window.location.href = 'https://www.hotstar.com/in/movies/spider-man-2/1260105551?filters=content_type%3Dmovie';
+  }
+
+  function navigate39() {
+    window.location.href = 'https://www.hotstar.com/in/movies/spider-man-3/1260105553?filters=content_type%3Dmovie';
+  }
+
+  function navigate40() {
+    window.location.href = 'https://www.hotstar.com/in/movies/the-amazing-spider-man/1260104625?filters=content_type%3Dmovie';
+  }
+
+  function navigate41() {
+    window.location.href = 'https://www.hotstar.com/in/movies/venom/1260104627?filters=content_type%3Dmovie';
+  }
+
+  function navigate42() {
+    window.location.href = '#';
+  }
+
+  function navigate43() {
+    window.location.href = 'https://www.hotstar.com/in/movies/spider-man-homecoming/1260104626?filters=content_type%3Dmovie';
+  }
+
+  function navigate44() {
+    window.location.href = 'https://www.sonyliv.com/movies/spider-man-far-from-home-1000214938';
+  }
+
+  function navigate45() {
+    window.location.href = 'https://www.sonyliv.com/movies/spider-man-no-way-home-1000224331';
+  }
+
+  function navigate46() {
+    window.location.href = '#';
+  }
+
+  function navigate47() {
+    window.location.href = '#';
+  }
